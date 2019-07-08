7dc456d645ab457cb62cec0e4f8f62218c94f280
jomasim <simjoms@gmail.com>
2019-07-08 11:25:22 +0300

    intial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View,Text} from 'react-native'
+import React, {Fragment} from 'react';
+const App = () => {
+  return (
+    <Fragment>
+     <View>
+       <Text>Hello welcome to carin</Text>
+     </View>
+    </Fragment>
+  );
+};
+
+export default App;
